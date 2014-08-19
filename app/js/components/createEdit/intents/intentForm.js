dc17d0c05d4eaa238131641b80fa3083e281eb38
Rob Szewczyk <rob@gmail.com>
2014-08-19 16:25:11 -0400

    Adding remove buttons to the intent and contact forms

--- app/js/components/createEdit/intents/intentForm.js
+++ app/js/components/createEdit/intents/intentForm.js
@@ -31,8 +31,13 @@ var IntentForm = React.createClass({
         return (
             /*jshint ignore: start */
             <div>
-                <Dropdown options={actions} ref="action" value={action} />
-                <Dropdown options={types} ref="dataType" value={dataType} />
+                <div className="col-sm-10">
+                    <Dropdown options={actions} ref="action" value={action} />
+                    <Dropdown options={types} ref="dataType" value={dataType} />
+                </div>
+                <div className="col-sm-2">
+                    <button className="btn btn-primary" onClick={this.props.removeHandler}>Remove</button>
+                </div>
             </div>
             /*jshint ignore: end */
         );
