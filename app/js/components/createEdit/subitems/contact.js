4c88c5f6f1f855954c0864b33d2b89e780a6e6c9
Rob Szewzcyk <rob.szew@gmail.com>
2014-08-24 17:43:06 -0400

    validation for the contact form

--- app/js/components/createEdit/subitems/contact.js
+++ app/js/components/createEdit/subitems/contact.js
@@ -8,6 +8,10 @@ var React            = require('react'),
     DeleteBtnMixin   = require('./deleteBtnMixin'),
     ConfigStoreMixin = require('../../../stores/ConfigStore').mixin;
 
+var phoneRegex = /(^\+\d((([\s.-])?\d+)?)+$)|(^(\(\d{3}\)\s?|^\d{3}[\s.-]?)?\d{3}[\s.-]?\d{4}$)/,
+    NEED_ONE_PHONE = 'Please provide at least one valid phone number',
+    INVALID_PHONE = 'Please enter a valid phone number (e.g. 555-5555, 555-555-5555)';
+
 module.exports.form = React.createClass({
     mixins: [DeleteBtnMixin, ConfigStoreMixin],
 
@@ -22,50 +26,77 @@ module.exports.form = React.createClass({
         var contact = this.props.item;
         /*jshint ignore:start */
         return (
-            <div className="row contact-card" onBlur={this.handleBlur} onFocus={this.handleFocus}>
+            <div className="row contact-card">
                 <div className="col-sm-12">
                     {!this.props.locked && this.renderDeleteBtn()}
-                    <Input elementType="select" required options={contactTypes} itemValue={contact.type.id} label="Contact Type" />
-                    <Input elementType="input" required type="text" itemValue={contact.name} label="Name" />
+                    <Input elementType="select" options={contactTypes} itemValue={contact.type.id} label="Contact Type" required />
+                    <Input elementType="input" type="text" itemValue={contact.name} label="Name" required />
                     <Input elementType="input" type="text" itemValue={contact.organization} label="Organization" />
-                    <Input elementType="input" required type="email" itemValue={contact.email} label="Email" />
-                    <Input elementType="input" type="text" ref="securePhone" itemValue={contact.securePhone} label="Secure Phone" error={this.state.phoneError} />
-                    <Input elementType="input" type="text" ref="unsecurePhone" itemValue={contact.unsecurePhone} label="Unsecure Phone" error={this.state.phoneError} />
+                    <Input elementType="input" type="email" ref="email" itemValue={contact.email} label="Email" required />
+                    <Input elementType="input" type="text" ref="securePhone" itemValue={contact.securePhone}
+                            label="Secure Phone" error={this.state.securePhoneError}
+                            onFocus={this.validatePhone.bind(this, 'secure')} onBlur={this.validatePhone.bind(this, 'secure')} />
+                    <Input elementType="input" type="text" ref="unsecurePhone" itemValue={contact.unsecurePhone}
+                            label="Unsecure Phone" error={this.state.unsecurePhoneError}
+                            onFocus={this.validatePhone.bind(this, 'unsecure')} onBlur={this.validatePhone.bind(this, 'unsecure')} />
                 </div>
             </div>
         );
         /*jshint ignore:end */
     },
 
-    handleFocus: function (event) {
-        var focused = event.target,
-            securePhoneDOM = this.refs.securePhone.getDOMNode(),
-            unsecurePhoneDOM = this.refs.unsecurePhone.getDOMNode();
+    validatePhone: function (refName, event) {
+        var secure = this.props.item.securePhone.val(),
+            unsecure = this.props.item.unsecurePhone.val(),
+            secureBlank = (!secure || secure === ''),
+            unsecureBlank = (!unsecure || unsecure === '');
 
-        if (securePhoneDOM.contains(focused) || unsecurePhoneDOM.contains(focused)) {
-            this.setState({phoneError: null});
+        var state = {
+            securePhoneError: this.state.securePhoneError,
+            unsecurePhoneError: this.state.unsecurePhoneError
+        };
+
+        //if leaving a phone input element, check that both are not blank
+        //and that both are valid phone numbers
+        if (event.type === 'blur') {
+            if (secureBlank && unsecureBlank) {
+                state.securePhoneError = NEED_ONE_PHONE;
+                state.unsecurePhoneError = NEED_ONE_PHONE;
+            } else {
+                state.securePhoneError = phoneRegex.test(secure) || secureBlank ? null : INVALID_PHONE;
+                state.unsecurePhoneError = phoneRegex.test(unsecure) || unsecureBlank ? null : INVALID_PHONE;
+            }
         }
-    },
 
-    handleBlur: function (event) {
-        var blurred = event.target,
-            securePhoneDOM = this.refs.securePhone.getDOMNode(),
-            unsecurePhoneDOM = this.refs.unsecurePhone.getDOMNode(),
-            securePhoneVal = this.refs.securePhone.value(),
-            unsecurePhoneVal = this.refs.unsecurePhone.value();
-
-        var leavingAPhoneField = securePhoneDOM.contains(blurred) || unsecurePhoneDOM.contains(blurred),
-            phoneFieldsAreBlank = (!securePhoneVal || securePhoneVal === '') && (!unsecurePhoneVal || unsecurePhoneVal === '');
-
-        if (leavingAPhoneField && phoneFieldsAreBlank) {
-            this.setState({phoneError: 'Please provide at least one valid phone number'});
+        //if focusing on a phone element when the 'both are blank' error state exists,
+        //set the invalid error on the element receiving focus and clear the other
+        if (event.type === 'focus' && this.state.securePhoneError === NEED_ONE_PHONE) {
+            state.securePhoneError = refName === 'secure' ? INVALID_PHONE : null;
+            state.unsecurePhoneError = refName === 'unsecure' ? INVALID_PHONE : null;
         }
+
+        //on change clear any errors that are resolved with the change
+        if (event.type === 'change') {
+            if (this.state.securePhoneError && phoneRegex.test(secure)) {
+                state.securePhoneError = null;
+            }
+
+            if (this.state.unsecurePhoneError && phoneRegex.test(unsecure)) {
+                state.unsecurePhoneError = null;
+            }
+        }
+
+        this.setState(state);
     },
 
     getInitialState: function () {
-        return {
-            phoneError: null
-        };
+        return {unsecurePhoneError: null, securePhoneError: null};
+    },
+
+    componentWillReceiveProps: function () {
+        //validate phone needs to be called somewhere it has access to the old
+        //state and where it's safe to set state
+        this.validatePhone(null, {type: 'change'});
     }
 });
 
