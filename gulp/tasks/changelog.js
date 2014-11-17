9a3b4853276605760c8b893280462229e636c680
Nish <nishantpatel611@gmail.com>
2014-11-17 14:06:28 -0500

    chore(Changelog): write to CHANGELOG file

--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
+var fs = require('fs');
 var changelog = require('conventional-changelog');
 var config = require('../../package.json');
 
 gulp.task('changelog', function () {
     changelog({
         repository: config.repository.url,
-        version: config.version
+        version: config.version,
+        file: 'CHANGELOG.md'
     }, function(err, log) {
         if (err) {
             throw err;
         }
         console.log('Changelog is written to CHANGELOG.md file.');
+        fs.writeFileSync('CHANGELOG.md', log);
     });
 });
