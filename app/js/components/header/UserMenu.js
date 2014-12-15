500e4bc68507fe0b2c62e5ec9b359a024da1774d
Matt Wisniewski <contact.mattdylan@gmail.com>
2014-12-15 14:25:20 -0500

    fix(list-unstyled): Swapped for bootstrap list-unstyled

--- app/js/components/header/UserMenu.js
+++ app/js/components/header/UserMenu.js
@@ -25,29 +25,29 @@ var UserMenu = React.createClass({
               <ul className="dropdown-menu" role="menu">
               <li>
                 <h4>Personalize</h4>
-                <ul>
+                <ul className="list-unstyled">
                   <li><a href="http://ozone-development.github.io/hud-ui/"><i className="icon-ribbon"></i> Bookmarks</a></li>
                   <li><a href="#profile"><i className="icon-head"></i> Profile</a></li>
                   <li><a href="#settings"><i className="icon-cog"></i> Settings</a></li>
                 </ul>
               </li>
               <li>
-              <h4>Create</h4>
-                <ul>
+                <h4>Create</h4>
+                <ul className="list-unstyled">
                   <li><Link to="edit"><i className="icon-square-plus"></i> Submit a Listing</Link></li>
                   <li><a href="#new"><i className="icon-box"></i> Create a Suite</a></li>
                 </ul>
               </li>
               <li>
-              <h4>Manage</h4>
-                <ul>
+                <h4>Manage</h4>
+                <ul className="list-unstyled">
                   <li><Link to="my-listings"><i className="icon-stack"></i> Listing Managment</Link></li>
                   <li><Link to="mall-management"><i className="icon-shopping-settings"></i> AppsMall Management</Link></li>
                   <li><a href={METRICS_URL}><i className="icon-bar-graph"></i> Metrics</a></li>
                 </ul>
               </li>
               <li>
-                <ul className="goback">
+                <ul className="goback list-unstyled">
                   <li className="backToHud">
                     <a href="http://ozone-development.github.io/hud-ui/"><i className="icon-arrow-right"></i> Back to HUD</a>
                   </li>
