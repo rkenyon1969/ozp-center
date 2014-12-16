2d5ffe4ec9fb3f2c2ebfdf92ee000b1404c5a30f
Matt Wisniewski <contact.mattdylan@gmail.com>
2014-12-16 10:51:54 -0500

    fix(User-Menu.js): OZF-650 Fixed indentation

--- app/js/components/header/UserMenu.js
+++ app/js/components/header/UserMenu.js
@@ -19,47 +19,53 @@ var UserMenu = React.createClass({
         /*jshint ignore:start */
         return (
             <div id="user-menu" className="dropdown navbar-right">
-              <a className="btn dropdown-toggle" data-toggle="dropdown" href="#">
-                <i className="fa fa-bars"></i>
-              </a>
-              <ul className="dropdown-menu" role="menu">
-              <li>
-                <h4>Personalize</h4>
-                <ul className="list-unstyled">
-                  <li><a href="http://ozone-development.github.io/hud-ui/"><i className="icon-ribbon"></i> Bookmarks</a></li>
-                  <li><a href="#profile"><i className="icon-head"></i> Profile</a></li>
-                  <li><a href="#settings"><i className="icon-cog"></i> Settings</a></li>
-                </ul>
-              </li>
-              <li>
-                <h4>Create</h4>
-                <ul className="list-unstyled">
-                  <li><Link to="edit"><i className="icon-square-plus"></i> Submit a Listing</Link></li>
-                  <li><a href="#new"><i className="icon-box"></i> Create a Suite</a></li>
-                </ul>
-              </li>
-              <li>
-                <h4>Manage</h4>
-                <ul className="list-unstyled">
-                  <li><Link to="my-listings"><i className="icon-stack"></i> Listing Managment</Link></li>
-                  <li><Link to="mall-management"><i className="icon-shopping-settings"></i> AppsMall Management</Link></li>
-                  <li><a href={METRICS_URL}><i className="icon-bar-graph"></i> Metrics</a></li>
-                </ul>
-              </li>
-              <li>
-                <ul className="goback list-unstyled">
-                  <li className="backToHud">
-                    <a href="http://ozone-development.github.io/hud-ui/"><i className="icon-arrow-right"></i> Back to HUD</a>
-                  </li>
-                </ul>
-              </li>
+                <a className="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                    <i className="fa fa-bars"></i>
+                </a>
+                <ul className="dropdown-menu" role="menu">
+                <li>
+                    <h4>Personalize</h4>
+                    <ul className="list-unstyled">
+                        <li><a href="http://ozone-development.github.io/hud-ui/"><i className="icon-ribbon"></i> Bookmarks</a></li>
+                        <li><a href="#profile"><i className="icon-head"></i> Profile</a></li>
+                        <li><a href="#settings"><i className="icon-cog"></i> Settings</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>Create</h4>
+                    <ul className="list-unstyled">
+                        <li><Link to="edit"><i className="icon-square-plus"></i> Submit a Listing</Link></li>
+                        {
+                            this.state.currentUser.isAdmin &&
+                            <li><a href="#new"><i className="icon-box"></i> Create a Suite</a></li>
+                        }
+                    </ul>
+                </li>
+                <li>
+                    <h4>Manage</h4>
+                    <ul className="list-unstyled">
+                        <li><Link to="my-listings"><i className="icon-stack"></i> Listing Managment</Link></li>
+                        {
+                            this.state.currentUser.isAdmin &&
+                            <li><Link to="mall-management"><i className="icon-shopping-settings"></i> AppsMall Management</Link></li>
+                        }
+                        <li><a href={METRICS_URL}><i className="icon-bar-graph"></i> Metrics</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="goback list-unstyled">
+                        <li className="backToHud">
+                            <a href="http://ozone-development.github.io/hud-ui/"><i className="icon-arrow-right"></i> Back to HUD</a>
+                        </li>
+                    </ul>
+                </li>
 
-            </ul>
+                </ul>
                 {
                     this.state.showHelp ?
-                        <Modal className="HelpModal" title="OZONE Help Zone" onHidden={this.onModalHidden}>
-                            <iframe style={{width:"100%", height:"500px", border: 'none'}} src={HELP_URL} />
-                        </Modal> : null
+                    <Modal className="HelpModal" title="OZONE Help Zone" onHidden={this.onModalHidden}>
+                    <iframe style={{width:"100%", height:"500px", border: 'none'}} src={HELP_URL} />
+                    </Modal> : null
                 }
             </div>
         );
