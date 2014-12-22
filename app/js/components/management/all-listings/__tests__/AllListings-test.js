b3c93cf5497219113a535c0b07c7240574b76510
Stephanie Schneider <sschneider713@gmail.com>
2014-12-22 15:32:28 -0500

    chore(Unit Tests): working all listings sidebar unit tests

--- app/js/components/management/all-listings/__tests__/AllListings-test.js
+++ app/js/components/management/all-listings/__tests__/AllListings-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('AllListings', function() {
-    it('', function() {
+    it('renders the sidebar on the all listings page', function() {
         var React = require('react');
         var AllListings = require('../index.js');
         var expect = require('chai').expect;
@@ -9,7 +9,19 @@ describe('AllListings', function() {
 
         var allListings = TestUtils.renderIntoDocument(React.createElement(AllListings));
 
-        var html = TestUtils.findRenderedDOMComponentWithTag(allListings, 'form');
-        expect(html.getDOMNode().find('radioGroup[name="approvalStatus"]')).to.exist;
+        expect($(allListings.getDOMNode()).find('.AllListings__sidebar')[0]).to.exist();
+
+    });
+
+    it('renders the grid view on the all listings page', function() {
+        var React = require('react');
+        var AllListings = require('../index.js');
+        var expect = require('chai').expect;
+        var TestUtils = React.addons.TestUtils;
+
+        var allListings = TestUtils.renderIntoDocument(React.createElement(AllListings));
+
+        expect($(allListings.getDOMNode()).find('.AllListings__listings')[0]).to.exist();
+
     });
 });
--- app/js/components/management/all-listings/__tests__/AllListingsSidebar-test.js
+++ app/js/components/management/all-listings/__tests__/AllListingsSidebar-test.js
@@ -19,4 +19,60 @@ describe('AllListingsSidebar', function() {
         var html = TestUtils.findRenderedDOMComponentWithTag(allListingsSidebar, 'form');
         expect($(html.getDOMNode()).find('radioGroup[name="approvalStatus"]')).to.exist();
     });
+
+    it('renders filters for each organization', function() {
+        var AllListingsSidebar = require('../AllListingsSidebar');
+        var allListingsSidebar = TestUtils.renderIntoDocument(
+            <AllListingsSidebar
+                counts = { {
+                    'APPROVLED': 1,
+                    'organizations': {
+                        '1': 1
+                    },
+                    'total': 1,
+                    'enabled': 1
+                } }
+                listings = { [] }
+                value = { {approvalStatus: null, org: null, enabled: null} }
+                organizations = { [
+                    {
+                    'id' : 1,
+                    'shortName' : 'TO1',
+                    'title' : 'Test Organization'
+                    }
+                ] }
+            />
+        );
+
+        var html = TestUtils.findRenderedDOMComponentWithTag(allListingsSidebar, 'form');
+        expect($(html.getDOMNode()).find('input[id="all-listings-filter-organization-to1"]')).to.exist();
+    });
+
+    it('renders counts for each filter', function() {
+        var AllListingsSidebar = require('../AllListingsSidebar');
+        var allListingsSidebar = TestUtils.renderIntoDocument(
+            <AllListingsSidebar
+                counts = { {
+                    'APPROVLED': 1,
+                    'organizations': {
+                        '1': 1
+                    },
+                    'total': 1,
+                    'enabled': 1
+                } }
+                listings = { [] }
+                value = { {approvalStatus: null, org: null, enabled: null} }
+                organizations = { [
+                    {
+                    'id' : 1,
+                    'shortName' : 'TO1',
+                    'title' : 'Test Organization'
+                    }
+                ] }
+            />
+        );
+        var html = $(allListingsSidebar.getDOMNode()).find('label[class="label-enabled"]')[0];
+        expect($(html).find('strong[class="badge"]').text()).to.equal('1');
+    });
+    
 });
