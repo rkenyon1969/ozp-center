aed48fba686e742d86638004555a440462ad8a8b
Sarah Deighan <sarah.deighan@nextcentury.com>
2015-01-22 10:12:15 -0500

    Updating edit button colors
    Updated edit button color scheme to match other Detailed View buttons

--- app/js/components/quickview/Header.js
+++ app/js/components/quickview/Header.js
@@ -52,7 +52,7 @@ var QuickviewHeader = React.createClass({
             <div className="btn-group quickview-header-actions">
                 <button type="button" className="btn btn-default" onClick={ this.launch }><i className="fa fa-external-link"></i></button>
                 <BookmarkButton listing={this.props.listing} />
-                {this.props.allowEdit && <button type="button" className="btn btn-primary" onClick={ this.props.onEdit }><i className="fa fa-edit"></i></button>}
+              {this.props.allowEdit && <button type="button" className="btn btn-default" onClick={ this.props.onEdit }><i className="fa fa-edit"></i></button>}
             </div>
         );
         /* jshint ignore:end */
