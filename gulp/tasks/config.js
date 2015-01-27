3e9f2cc2a52a77093dbb036a3f6c92803e5e4a24
Ross Pokorny <ross.pokorny@nextcentury.com>
2015-01-27 12:40:52 -0700

    chore(External Config): OZF-863 Fix gulp configuration

--- gulp/tasks/config.js
+++ gulp/tasks/config.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var preprocess = require('gulp-preprocess');
 
-gulp.task('copy', function() {
+gulp.task('config', function() {
     return gulp.src(['app/OzoneConfig.js'])
         .pipe(preprocess())
         .pipe(gulp.dest('dist'));
