b75bf8e88099e97f263005e6bfed4d49c608a7a1
Stephanie Schneider <sschneider713@gmail.com>
2015-02-03 13:26:40 -0500

    chore(Style): Update tabs

--- app/js/components/shared/__tests__/ChangeLog-test.js
+++ app/js/components/shared/__tests__/ChangeLog-test.js
@@ -13,53 +13,50 @@ describe('ChangeLog', function () {
     var expect = require('chai').expect;
 
     it('renders a change log with no listing name', function () {
-      var changeLog = TestUtils.renderIntoDocument(
-        <ChangeLog
-          showListingName={false}
-          changeLog={
-            {
-              "id":112,
-              "action":"APPROVED",
-              "author": {
-                "id":2,
-                "displayName":"Test Admin 1",
-                "username":"testAdmin1"
-                },
-              "listing":{
-                  "id":28,
-                  "iconUrl":"https://localhost:8443/marketplace/api/image/cacbb033-40f1-49ac-9e53-b964b968a92f.png",
-                  "title":"FrameIt",
-                  "agency":"Test Organization"
-              },
-              "activityDate":"2015-01-30T17:43:45.365+0000",
-              "changeDetails":[]
-            }}
-        />
-      );
+        var changeLog = TestUtils.renderIntoDocument(
+            <ChangeLog
+                showListingName={false}
+                changeLog={{
+                "id":112,
+                "action":"APPROVED",
+                "author": {
+                    "id":2,
+                    "displayName":"Test Admin 1",
+                    "username":"testAdmin1"
+                    },
+                "listing":{
+                    "id":28,
+                    "iconUrl":"https://localhost:8443/marketplace/api/image/cacbb033-40f1-49ac-9e53-b964b968a92f.png",
+                    "title":"FrameIt",
+                    "agency":"Test Organization"
+                    },
+                "activityDate":"2015-01-30T17:43:45.365+0000",
+                "changeDetails":[]
+                }}
+            />);
 
-      expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:first-child').text()).to.equal('Test Admin 1');
-      expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:nth-child(3)').text()).to.equal('approved');
-      expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:last-child').text()).to.equal('the listing');
+        expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:first-child').text()).to.equal('Test Admin 1');
+        expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:nth-child(3)').text()).to.equal('approved');
+        expect($(changeLog.getDOMNode()).find('div.col-md-9 > div > span:last-child').text()).to.equal('the listing');
 
     });
 
     it('renders a change log with a listing name', function() {
-      var changeLog;
-      var routes = createRoutes(ChangeLog);
-      TestLocation.history = ['/test'];
-      var router = Router.run(routes, TestLocation, function (Handler) {
+        var changeLog;
+        var routes = createRoutes(ChangeLog);
+        TestLocation.history = ['/test'];
+        var router = Router.run(routes, TestLocation, function (Handler) {
         changeLog = TestUtils.renderIntoDocument(
-          <Handler
-            showListingName={true}
-            changeLog={
-              {
+            <Handler
+                showListingName={true}
+                changeLog={{
                 "id":112,
                 "action":"APPROVED",
                 "author": {
-                  "id":2,
-                  "displayName":"Test Admin 1",
-                  "username":"testAdmin1"
-                  },
+                    "id":2,
+                    "displayName":"Test Admin 1",
+                    "username":"testAdmin1"
+                    },
                 "listing":{
                     "id":28,
                     "iconUrl":"https://localhost:8443/marketplace/api/image/cacbb033-40f1-49ac-9e53-b964b968a92f.png",
@@ -68,15 +65,14 @@ describe('ChangeLog', function () {
                 },
                 "activityDate":"2015-01-30T17:43:45.365+0000",
                 "changeDetails":[]
-              }}
-          />
-        );
-      });
+                }}
+            />);
+        });
 
-      expect($(changeLog.getDOMNode()).find('a').text()).to.equal('FrameIt');
-      router.teardown();
+        expect($(changeLog.getDOMNode()).find('a').text()).to.equal('FrameIt');
+        router.teardown();
 
-    });
+});
 
 
 
