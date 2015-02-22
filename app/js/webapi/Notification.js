4f323f24adc489107b8449b4aea8f7ce8c186cc9
Nish <nishantpatel611@gmail.com>
2015-02-22 14:26:07 -0500

    fix(Notifications): fix Date parsing in IE11 and Firefox

--- app/js/webapi/Notification.js
+++ app/js/webapi/Notification.js
@@ -7,7 +7,7 @@ var { PAGINATION_MAX } = require('ozp-react-commons/constants');
 var PaginatedResponse  =require ('./responses/PaginatedResponse');
 
 function parse (json) {
-    json.expiresDate = new Date(json.expiresDate.replace('+0000', ''));
+    json.expiresDate = new Date(json.expiresDate.replace('+0000', 'Z'));
     return json;
 }
 
