2f775f0fa1bea39502cbff8f8a8997a723929c8a
Nish <nishantpatel611@gmail.com>
2015-03-02 12:35:34 -0500

    chore(Router): fix tests

--- app/js/components/management/user/__tests__/MyListings-test.js
+++ app/js/components/management/user/__tests__/MyListings-test.js
@@ -6,27 +6,27 @@ var Router = require('react-router');
 var $ = require('jquery');
 var { TestUtils } = React.addons;
 var Routes = require('../../../../components/Routes.jsx');
-var TestLocation = require('react-router/modules/locations/TestLocation');
+var TestLocation = require('react-router/lib/locations/TestLocation');
 var ProfileMock = require('../../../../__tests__/mocks/ProfileMock');
 var createRoutes = require('../../../../__tests__/createRoutes');
 
 describe('MyListings', function () {
     var ListingManagement = require('../MyListings.jsx');
-    var routes, userMenu, router;
+    var routes, userMenu, router, location;
 
     beforeEach(function () {
         routes = createRoutes(ListingManagement);
-        TestLocation.history = ['/test'];
+        location = new TestLocation(['/test']);
     });
 
     it('renders my listings sidebar', function () {
         ProfileMock.mockUser();
         var listingManagement;
-        var router = Router.run(routes, TestLocation, function (Handler) {
+        var router = Router.run(routes, location, function (Handler) {
             listingManagement = TestUtils.renderIntoDocument(<Handler />);
         });
         expect($(listingManagement.getDOMNode()).find('.Listings__Sidebar')[0]).to.exist;
-        router.teardown();
+        router.stop();
 
         ProfileMock.restore();
     });
--- app/js/components/management/user/__tests__/RecentActivity-test.js
+++ app/js/components/management/user/__tests__/RecentActivity-test.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var Router = require('react-router');
-var TestLocation = require('react-router/modules/locations/TestLocation');
+var TestLocation = require('react-router/lib/locations/TestLocation');
 var createRoutes = require('../../../../__tests__/createRoutes');
 var ProfileMock = require('../../../../__tests__/mocks/ProfileMock');
 
 
 
 describe('RecentActivity', function() {
+    var location = new TestLocation(['/test']);
+
     it('renders the recent activity sidebar', function() {
         var React = require('react');
         var RecentActivity = require('../RecentActivity.jsx');
@@ -16,14 +18,13 @@ describe('RecentActivity', function() {
         var recentActivity;
 
         var routes = createRoutes(RecentActivity);
-        TestLocation.history = ['/test'];
 
-        var router = Router.run(routes, TestLocation, function (Handler) {
+        var router = Router.run(routes, location, function (Handler) {
             recentActivity = TestUtils.renderIntoDocument(<Handler />);
         });
 
         expect($(recentActivity.getDOMNode()).find('.RecentActivity__Sidebar')[0]).to.exist();
-        router.teardown();
+        router.stop();
     });
 
     it('renders the recent activity list', function() {
@@ -34,14 +35,13 @@ describe('RecentActivity', function() {
         var recentActivity;
 
         var routes = createRoutes(RecentActivity);
-        TestLocation.history = ['/test'];
 
-        var router = Router.run(routes, TestLocation, function (Handler) {
+        var router = Router.run(routes, location, function (Handler) {
             recentActivity = TestUtils.renderIntoDocument(<Handler />);
         });
 
         expect($(recentActivity.getDOMNode()).find('.RecentActivity__activities')[0]).to.exist();
-        router.teardown();
+        router.stop();
     });
 
     it('renders the organization steward filters', function() {
@@ -52,16 +52,15 @@ describe('RecentActivity', function() {
         var recentActivity;
 
         var routes = createRoutes(RecentActivity);
-        TestLocation.history = ['/test'];
 
         ProfileMock.mockOrgSteward();
-        var router = Router.run(routes, TestLocation, function (Handler) {
+        var router = Router.run(routes, location, function (Handler) {
             recentActivity = TestUtils.renderIntoDocument(<Handler />);
         });
 
         expect($(recentActivity.getDOMNode()).find('RadioGroup[name="recent-activity-org-listings"]')).to.exist();
 
-        router.teardown();
+        router.stop();
         ProfileMock.restore();
     });
 
@@ -73,16 +72,15 @@ describe('RecentActivity', function() {
         var recentActivity;
 
         var routes = createRoutes(RecentActivity);
-        TestLocation.history = ['/test'];
 
         ProfileMock.mockAdmin();
-        var router = Router.run(routes, TestLocation, function (Handler) {
+        var router = Router.run(routes, location, function (Handler) {
             recentActivity = TestUtils.renderIntoDocument(<Handler />);
         });
 
         expect($(recentActivity.getDOMNode()).find('RadioGroup[name="recent-activity-all-listings"]')).to.exist();
 
-        router.teardown();
+        router.stop();
         ProfileMock.restore();
     });
 
