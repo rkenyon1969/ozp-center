70582fc283500710ce13d01f7ba61a8a078833a4
Stephanie Schneider <sschneider713@gmail.com>
2015-03-09 10:15:54 -0400

    fix(Styling): #95 fixing navbar icons

--- app/js/components/NavBar/index.jsx
+++ app/js/components/NavBar/index.jsx
@@ -22,16 +22,16 @@ var NavBar = React.createClass({
 
     render: function () {
         var Metrics = (this.isAdmin() || this.isOrgSteward()) ?
-            <li><a href={METRICS_URL}><i className="icon-bar-graph-2"></i>Metrics</a></li> : '';
+            <li><a href={METRICS_URL}><i className="icon-bar-graph-2-grayLightest"></i>Metrics</a></li> : '';
 
         return (
             <nav className="navbar navbar-inverse navbar-fixed-top" id="globalNav">
                 <div className="container-fluid container" id="centered">
                     <div className="navbar-left">
                         <ul className="nav navbar-nav">
-                            <li><a className="lrg" href={HUD_URL}><i className="icon-home"></i></a></li>
-                            <li className="active"><a className="lrg" href='#/home' ><i className="icon-shopping activeIcon"></i></a></li>
-                            <li><a className="lrg" href={WEBTOP_URL}><i className="icon-layout"></i></a></li>
+                            <li><a className="lrg" href={HUD_URL}><i className="icon-home-grayLightest"></i></a></li>
+                            <li className="active"><a className="lrg" href='#/home' ><i className="icon-shopping-grayLightest activeIcon"></i></a></li>
+                            <li><a className="lrg" href={WEBTOP_URL}><i className="icon-layout-grayLightest"></i></a></li>
                         </ul>
                     </div>
 
@@ -39,33 +39,33 @@ var NavBar = React.createClass({
                         <ul className="nav navbar-nav">
                             <UserNotificationDropdown />
                             <li>
-                                <a onClick={this.showHelpModal}><i className="icon-question"></i></a>
+                                <a onClick={this.showHelpModal}><i className="icon-question-grayLightest"></i></a>
                             </li>
                             <li className="dropdown">
-                                <a href="#" className="lrg" data-toggle="dropdown"><i className="icon-menu"></i></a>
+                                <a href="#" className="lrg" data-toggle="dropdown"><i className="icon-menu-grayLightest"></i></a>
                                 <ul className="dropdown-menu">
                                     <li className="dropdown-header">Personalize</li>
-                                    <li><a href={HUD_URL}><i className="icon-ribbon"></i>Bookmarks</a></li>
+                                    <li><a href={HUD_URL}><i className="icon-ribbon-grayLightest"></i>Bookmarks</a></li>
                                     <li>
                                         <ProfileLink profileId="self">
-                                            <i className="icon-head"/>Profile
+                                            <i className="icon-head-grayLightest"/>Profile
                                         </ProfileLink>
                                     </li>
                                     <li>
                                         <ModalLink queryParams={{settings: true}}>
-                                            <i className="icon-cog"></i>Settings
+                                            <i className="icon-cog-grayLightest"></i>Settings
                                         </ModalLink>
                                     </li>
                                     <li className="divider"></li>
                                     <li className="dropdown-header">Create</li>
-                                    <li><a href={'#/edit'}><i className="icon-square-plus"></i>Submit a Listing</a></li>
-                                    <li><a href={DEVELOPER_RESOURCES_URL}><i className="icon-cloud"></i>Developer Resources</a></li>
+                                    <li><a href={'#/edit'}><i className="icon-square-plus-grayLightest"></i>Submit a Listing</a></li>
+                                    <li><a href={DEVELOPER_RESOURCES_URL}><i className="icon-cloud-grayLightest"></i>Developer Resources</a></li>
                                     <li className="divider"></li>
                                     <li className="dropdown-header">Manage</li>
-                                    <li><a href={'#/user-management/my-listings'}><i className="icon-layers"></i>Listing Management</a></li>
+                                    <li><a href={'#/user-management/my-listings'}><i className="icon-layers-grayLightest"></i>Listing Management</a></li>
                                     {
                                         this.isAdmin() &&
-                                        <li><a href={'#/mall-management/categories'}><i className="icon-shopping-settings"></i>Marketplace Settings</a></li>
+                                        <li><a href={'#/mall-management/categories'}><i className="icon-shopping-settings-grayLightest"></i>Marketplace Settings</a></li>
                                     }
                                     { Metrics }
                                 </ul>
