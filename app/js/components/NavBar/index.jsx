fa8308085208d85f74234b572e01e0b5574ae547
Sarah Deighan <sarah.deighan@nextcentury.com>
2015-04-14 13:12:04 -0400

    chore(cleaning): Removing old variable declarations. Matching help buttons
    Removed unnecessary variable definitions and making the help button
    code match HUD

--- app/js/components/NavBar/index.jsx
+++ app/js/components/NavBar/index.jsx
@@ -38,7 +38,7 @@ var NavBar = React.createClass({
                         <ul className="nav navbar-nav">
                             <UserNotificationDropdown />
                             <li>
-                                <a onClick={this.showHelpModal}><i className="icon-question-grayLightest"></i></a>
+                                <a href="#" onClick={this.showHelpModal}><i className="icon-question-grayLightest"></i></a>
                             </li>
                             <li className="dropdown user-menu-dropdown">
                                 <a href="#" className="lrg" data-toggle="dropdown"><i className="icon-menu-grayLightest"></i></a>
