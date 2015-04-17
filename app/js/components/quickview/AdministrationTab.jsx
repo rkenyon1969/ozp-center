a341b076f4dd4eeca568b5aa580d577b77833d20
Sarah Deighan <sarah.deighan@nextcentury.com>
2015-04-17 13:04:24 -0400

    fix(styling): #201 Admin tab styles reflect MP & Org Stewards
    Element styles separated for MP Steward and Org Steward in the Admin
    tab in Detailed View Modal

--- app/js/components/quickview/AdministrationTab.jsx
+++ app/js/components/quickview/AdministrationTab.jsx
@@ -142,12 +142,19 @@ var AdministrationTab = React.createClass({
                 iconClass= 'icon-thumbs-up-14';
                 break;
             case 'Pending, Organization':
-                if (isStewardOfOrg || isAdmin) {
+                if (isStewardOfOrg) {
                     controls = this.renderReviewSection();
                     statusClass = 'label-needs-action';
                     iconClass= 'icon-exclamation-14';
 
-                } else {
+                }
+                if (isAdmin) {
+                    controls = this.renderReviewSection();
+                    statusClass = 'label-pending';
+                    iconClass= 'icon-loader-14';
+
+                }
+                else {
                     controls = [];
                     statusClass = 'label-pending';
                     iconClass= 'icon-loader-14';
@@ -205,6 +212,10 @@ var AdministrationTab = React.createClass({
             }}}
         );
 
+        var isAdmin = UserRole[this.props.currentUser.highestRole] >= UserRole.ADMIN,
+            isStewardOfOrg = _.contains(this.props.currentUser.stewardedOrganizations, this.props.listing.agency),
+            pendingOrg = (listingStatus[this.props.listing.approvalStatus]=='Pending, Organization') ? true : false;
+
         if (editing) {
             return (
                 <section className="return-feedback">
@@ -217,7 +228,18 @@ var AdministrationTab = React.createClass({
                     </form>
                 </section>
             );
-        } else {
+        }
+        if( isAdmin && !isStewardOfOrg && pendingOrg) {
+            var org = this.props.listing.agencyShort;
+            return (
+                <section className="review-listing">
+                    <h5>{"Review Listing for " + org}</h5>
+                    <button type="button" className="btn btn-default" onClick={ this.approve }>{"Approve for " + org}</button>
+                    <button type="button" className="btn btn-default" onClick={ this.editRejection }>{"Reject for " + org}</button>
+                </section>
+            );
+        }
+        else {
             return (
                 <section className="review-listing">
                     <h5>Review Listing</h5>
