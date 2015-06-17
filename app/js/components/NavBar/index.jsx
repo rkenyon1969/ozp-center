54e226e56713c7cd90085d24ef07613d7d3e657e
wski <contact.mattdylan@gmail.com>
2015-06-17 14:26:56 -0400

    feat(Tooltips): Added tooltips to the global navbar #283

--- app/js/components/NavBar/index.jsx
+++ app/js/components/NavBar/index.jsx
@@ -21,6 +21,15 @@ var NavBar = React.createClass({
         };
     },
 
+    componentDidMount: function(){
+      $(this.refs.hastooltips.getDOMNode()).find('.tooltiped').each(function(){
+        $(this).tooltip({
+          delay: 400
+        });
+      });
+
+    },
+
     render: function () {
         var Metrics = (this.isAdmin() || this.isOrgSteward()) ?
             <li><a href={METRICS_URL} target="_blank"><i className="icon-bar-graph-2-grayLightest"></i>Metrics</a></li> : '';
@@ -29,23 +38,23 @@ var NavBar = React.createClass({
 
 
         return (
-            <nav className="navbar navbar-inverse navbar-fixed-top" id="globalNav">
+            <nav ref="hastooltips" className="navbar navbar-inverse navbar-fixed-top" id="globalNav">
                 <div className="container-fluid container" id="centered">
                     <div className="navbar-left">
                         <ul className="nav navbar-nav">
-                            <li><a className="lrg" href={HUD_URL}><i className="icon-home-grayLightest"></i></a></li>
-                            <li className="active"><a className="lrg" href='#/home' ><i className="icon-shopping-grayLightest activeIcon"></i></a></li>
-                            <li><a className="lrg" href={WEBTOP_URL}><i className="icon-layout-grayLightest"></i></a></li>
+                            <li className="tooltiped" data-toggle="tooltip" data-placement="bottom" title="HUD"><a className="lrg" href={HUD_URL}><i className="icon-home-grayLightest"></i></a></li>
+                            <li className="active tooltiped" data-toggle="tooltip" data-placement="bottom" title="Center"><a className="lrg" href='#/home' ><i className="icon-shopping-grayLightest activeIcon"></i></a></li>
+                            <li className="tooltiped" data-toggle="tooltip" data-placement="bottom" title="Webtop"><a className="lrg" href={WEBTOP_URL}><i className="icon-layout-grayLightest"></i></a></li>
                         </ul>
                     </div>
 
                     <div className="navbar-right">
                         <ul className="nav navbar-nav">
-                            <UserNotificationDropdown />
-                            <li>
+                            <UserNotificationDropdown/>
+                            <li className="tooltiped" data-toggle="tooltip" data-placement="bottom" title="Help">
                                 <a href="#" onClick={this.showHelpModal}><i className="icon-question-grayLightest"></i></a>
                             </li>
-                            <li className="dropdown user-menu-dropdown">
+                            <li data-toggle="tooltip" data-placement="bottom" title="Menu" className="dropdown user-menu-dropdown tooltiped">
                                 <a href="#" className="lrg" data-toggle="dropdown"><i className="icon-menu-grayLightest"></i></a>
                                 <ul className="dropdown-menu">
                                     <li className="dropdown-header">Personalize</li>
