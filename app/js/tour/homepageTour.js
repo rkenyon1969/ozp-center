07778fd6071c1f514be7bca11506475d7becb5de
bhalt <bhalteman@fearsol.com>
2015-10-13 14:26:04 -0400

    feat(tour): Update tour

--- app/js/tour/homepageTour.js
+++ app/js/tour/homepageTour.js
@@ -1,6 +1,7 @@
 module.exports = new Tour({
   backdrop: true,
   backdropPadding: 10,
+  template: "<div class='popover tour'><div class='arrow'></div><",
   steps: [
     {
       element: "#tourstop-hud",
