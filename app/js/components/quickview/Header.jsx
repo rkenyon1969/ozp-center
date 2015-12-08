8ab6c7bf69f542cea629ddf74aab3670b7b0c82f
SOAP1914 <RicardoEJames2@gmail.com>
2015-12-08 17:34:19 -0500

    (fix)The modal nav on each app

--- app/js/components/quickview/Header.jsx
+++ app/js/components/quickview/Header.jsx
@@ -30,12 +30,12 @@ var QuickviewHeader = React.createClass({
         return (
             <div className="quickview-header">
                 <div className="quickview-header-info">
-                    <button type="button" className="close" aria-hidden="true" onClick={ this.props.onCancel }><i className="icon-cross-16-grayDark"></i></button>
+                    <button type="button" className="close" aria-hidden="false" aria-label="close modal" onClick={ this.props.onCancel }><i className="icon-cross-16-grayDark"></i></button>
                     <img className="listing-icon" alt={`${listing.title} header information`} src={ image } data-fallback="/store/images/types/3" />
-                    <h3 className="listing-title" title={ title }>{ title }
+                    <h3 className="listing-title" tabIndex="0" title={ title }>{ title }
                     {
                         agencyShort &&
-                        <span className="company">{ agencyShort }</span>
+                        <span className="company">{ agencyShort } </span>
                     }
                     </h3>
                     <IconRating
@@ -56,13 +56,13 @@ var QuickviewHeader = React.createClass({
             <div ref="hastooltips" className="btn-group quickview-header-actions">
                 {
                     this.props.listing.approvalStatus === "APPROVED" &&
-                    <CenterLaunchLink listing={this.props.listing} className="btn btn-default"/>
+                    <CenterLaunchLink listing={this.props.listing} className="btn btn-default" aria-label={ `Launch ${this.props.listing.title}`} />
                 }
                 {
                     this.props.listing.approvalStatus === "APPROVED" &&
                     <BookmarkButton listing={this.props.listing} />
                 }
-              {this.props.allowEdit && <button data-toggle="tooltip" data-placement="top" title="Edit" type="button" className="tooltiped btn btn-default" onClick={ this.props.onEdit }><i className="icon-pencil-grayDark"></i></button>}
+              {this.props.allowEdit && <button data-toggle="tooltip" data-placement="top" title="Edit" type="button" aria-label={`Edit ${this.props.listing.title}`} className="tooltiped btn btn-default" onClick={ this.props.onEdit }><i className="icon-pencil-grayDark"></i></button>}
             </div>
         );
     }
