142de5cf9543c897c66132676c10168e9b0df36f
Alan Ward <arw180@gmail.com>
2015-12-21 14:42:51 -0500

    don't uglify for now

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -7,8 +7,8 @@ var WebpackDevServer = require('webpack-dev-server');
 gulp.task("webpack:prod", function (callback) {
     var prodConfig = Object.create(webpackConfig);
     prodConfig.plugins = prodConfig.plugins.concat(
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.DedupePlugin()// TODO put this back later,
+        // new webpack.optimize.UglifyJsPlugin()
     );
 
     // run webpack
