3a54c3ce9d5b9f0f5c4f4f3999cf5a3e2210e04a
bhalt <bhalteman@fearsol.com>
2015-12-23 11:10:36 -0500

    fix(help): Remove 5px of extra space

--- app/js/components/NavBar/helpmodal.jsx
+++ app/js/components/NavBar/helpmodal.jsx
@@ -54,17 +54,17 @@ var HelpModal = React.createClass({
                     <div className="modal-content">
                         <div className="modal-header">
                         <button type="button" className="close" data-dismiss="modal">
-                          <span aria-hidden="true"><i className="icon-cross-18"></i></span><span className="sr-only">Close</span>
+                          <span aria-hidden="true"><i className="icon-cross-16"></i></span><span className="sr-only">Close</span>
                         </button>
                         <h3 className="modal-title">Help</h3>
                         </div>
                         <div className="modal-body" style={{padding: '0px'}}>
-                            <iframe style={{width:"100%", height:"500px", border: 'none'}} src={`${HELP_URL}${this.state.role}`} />
+                            <iframe style={{width:"100%", height:"500px", display: "block", border: "none"}} src={`${HELP_URL}${this.state.role}`} />
                         </div>
                         <div className="modal-footer">
                             <h5>Have a question not answered here?&nbsp;&nbsp;
                                 <a href={HELPDESK_ADDRESS} type="button" className="btn btn-primary">Contact the Help Desk</a>
-                                {/*<button type="button" className="btn btn-primary" title="Coming Soon" disabled="disabled">Take the Center tour</button>*/}
+                                {/*<a type="button" className="btn btn-primary">Take the tour</a>*/}
                             </h5>
                         </div>
                     </div>
