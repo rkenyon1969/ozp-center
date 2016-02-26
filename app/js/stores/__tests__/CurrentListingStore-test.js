1de14bc0cb4c8e258f30cc01649edb7a4afd54cc
clarkjefcoat <clark.j.jefcoat@ugov.gov>
2016-02-26 08:42:02 -0500

    fix(typo): Added missing single quote

--- app/js/stores/__tests__/CurrentListingStore-test.js
+++ app/js/stores/__tests__/CurrentListingStore-test.js
@@ -119,7 +119,7 @@ describe('CurrentListingStore', function() {
 
         var imageSaveSpy = sinon.spy(function() {
                 return $.Deferred().reject({
-                    'responseText: {"security_marking": ["Security marking too high for current user"]}'
+                    'responseText': '{"security_marking": ["Security marking too high for current user"]}'
                 }, 'error', 'Image tooooo large').promise();
             }),
 
