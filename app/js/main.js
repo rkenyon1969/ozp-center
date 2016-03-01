22f8e86d33ae631dfc0e28fcae3996ed8bfde083
Matt Wisniewski <contact.mattdylan@gmail.com>
2016-03-01 12:16:28 -0500

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -112,29 +112,15 @@ if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
 
 function detectIE() {
     var ua = window.navigator.userAgent;
-
     var msie = ua.indexOf('MSIE ');
     if (msie > 0) {
         // IE 10 or older => return version number
         return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
     }
-
-    var trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-        // IE 11 => return version number
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-
-    var edge = ua.indexOf('Edge/');
-    if (edge > 0) {
-       // Edge (IE 12+) => return version number
-       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-    }
-
     // other browser
     return false;
 }
+
 if (detectIE() < 10) {
 alert(`
 OZP is tested against the following browsers:
